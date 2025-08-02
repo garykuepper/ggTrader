a1eba05640525b1d226993c881c1efb862b820f9
tr0n <tron@tron.com>
2025-08-02 15:19:02 -0700

    starting from scratch

--- minimals/dl_yfinance.py
+++ minimals/dl_yfinance.py
@@ -1,19 +1,46 @@
 import yfinance as yf
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
 
-df = yf.download("SPY", period="1y", interval="1d", progress=False, multi_level_index=False, auto_adjust=True)
-df.columns = df.columns.str.lower()
+def yf_download(symbol, interval, start_date, end_date):
+    df = yf.download(symbol,
+                     start=start_date,
+                     end=end_date,
+                     interval=interval,
+                     progress=False,
+                     multi_level_index=False,
+                     auto_adjust=True)
+
+    df['symbol'] = symbol
+    df['interval'] = interval
+    df['date'] = df.index
+    df.columns = df.columns.str.lower()
+
+    return df
+
+
+# Setup mongodb
+client = MongoClient('mongodb://localhost:27017/')
+db = client['learning']
+collection = db['stock_market_data']
+# collection.create_index([('symbol', 1), ('date', 1),('interval', 1)], unique=True)
+
+# Stock Data
+symbol = "SPY"
+interval = "1d"
+end_date = datetime(2025, 8, 1)
+start_date = end_date - timedelta(days=1)
+
+df = yf_download(symbol,
+                 interval,
+                 start_date.strftime("%Y-%m-%d"),
+                 end_date.strftime("%Y-%m-%d"))
 
 print(df.head())
 print(df.columns)
-
-db = client['learning']
-collection = db['stock_market_data']
-
 data = df.to_dict('records')
 
-result = collection.insert_many(data)
+# result = collection.insert_many(data)
 
-print(F"")
+# print(F"Num of ids: {len(result.inserted_ids)}")
