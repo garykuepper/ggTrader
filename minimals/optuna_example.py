e6fe7f49ed78fb008b7dafeed35ad6f6b83332af
garykuepper <gkuepper9@gmail.com>
2025-10-30 22:37:42 -0700

    Add `optuna` example with grid search and visualization, using `tabulate` for results formatting.

---
+++ minimals/optuna_example.py
@@ -0,0 +1,38 @@
+import optuna
+from optuna.samplers import GridSampler
+from optuna.visualization import plot_optimization_history, plot_param_importances
+from tabulate import tabulate
+# Define the objective function
+def objective(trial):
+    # Example parameters to optimize
+    x = trial.suggest_float("x", -10, 10)
+    y = trial.suggest_float("y", -10, 10)
+    # Simple quadratic function (minimum at x=3, y=-2)
+    return (x - 3)**2 + (y + 2)**2
+
+# Define the grid to search
+search_space = {
+    "x": [-10, -5, 0, 5, 10],
+    "y": [-10, -5, 0, 5, 10],
+}
+
+# Create a study with GridSampler
+sampler = GridSampler(search_space)
+study = optuna.create_study(sampler=sampler, direction="minimize")
+study.optimize(objective)
+
+# Print results
+print("Number of trials:", len(study.trials))
+print("Best trial:")
+print("  Value:", study.best_trial.value)
+print("  Params:", study.best_trial.params)
+trials = study.trials_dataframe()
+print(tabulate(trials, headers="keys", tablefmt="github"))
+
+# Plot optimization history
+fig1 = plot_optimization_history(study)
+fig1.show()
+
+# Plot parameter importances
+fig2 = plot_param_importances(study)
+fig2.show()
