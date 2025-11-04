c2e9b5d5f77b3d38ffd380109455fc160e4daf46
garykuepper <gkuepper9@gmail.com>
2025-11-03 22:19:05 -0800

    Update `try_remote.py` and `signal_explorer.ipynb`: adjust OHLCV retrieval interval, modify start time logic, and refine performance/stats visualizations with enhanced metrics and titles.

--- minimals/try_remote.py
+++ minimals/try_remote.py
@@ -6,6 +6,7 @@ k.use_remote("https://garygigabytes.com/kraken/parquet")  # no trailing slash
 df = k.read_parquet_remote(pair="BTC-USD", interval="1d")
 print(df.head())
 
-df_multi = k.get_ohlcv_df_remote(symbols=["BTC", "ETH", "AAVE"], interval="1d", quote="USD")
+df_multi = k.get_ohlcv_df_remote(symbols=["BTC", "ETH", "AAVE"], interval="1h", quote="USD")
 
 print(df_multi.head(10))
+print(df_multi.info())
