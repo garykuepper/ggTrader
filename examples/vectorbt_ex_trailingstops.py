cce45e685648da09371377173f26c85957619519
Gary Kuepper <65058102+garykuepper@users.noreply.github.com>
2025-11-08 12:12:38 -0800

    migrate vectorbt_backtest.py to vectorbt_ex.ipynb: replace script-based implementation with a Jupyter notebook for enhanced code modularity, detailed explanations, signal plotting, and streamlined backtesting workflows

--- examples/vectorbt_ex_trailingstops.py
+++ examples/vectorbt_ex_trailingstops.py
@@ -6,38 +6,52 @@ import pandas_ta as ta
 
 # ========= 1. Data =========
 tickers = ["BTC-USD", "ETH-USD"]
-data = vbt.YFData.download(tickers, start="2024-01-01")
+data = vbt.YFData.download(tickers, start="2025-01-01", interval='4h')
+
 close = data.get("Close").astype(np.float64).copy(deep=True)
 high  = data.get("High").astype(np.float64).copy(deep=True)
 low   = data.get("Low").astype(np.float64).copy(deep=True)
 
-
-atr = vbt.ta('AverageTrueRange').run(high, low, close, window=14)
-psar = vbt.ta('PSARIndicator').run(high, low,close, step=0.02, max_step=0.2)
-
-print(atr.average_true_range)
-print(psar.psar_up_indicator)
-
-atr_talib = vbt.talib('ATR').run(high, low, close, timeperiod=14)
-print(atr_talib.real)
-sar_talib = vbt.talib('SAR').run(high, low, acceleration=0.02, maximum=0.2)
-print(sar_talib.real)
-
+# ========= 2. Signal Data Comparison =========
+print(f"\npandas_ta")
 sar_pandas_ta = vbt.pandas_ta('psar').run(high, low, close=close, acceleration=0.02, maximum=0.2)
-print(sar_pandas_ta.psarl)
-buy = sar_pandas_ta.psarl_below(close)
-print(buy[buy['BTC-USD']])
+print(sar_pandas_ta.psarl.tail())
+sar_buy = sar_pandas_ta.psarl_below(close)
 
 adx_pandas_ta = vbt.pandas_ta('adx').run(high, low, close, length=14)
 
-buy2 = adx_pandas_ta.dmp_above(adx_pandas_ta.dmn)
-print(buy2.info())
-buy2.columns = buy2.columns.droplevel(0)
-print(buy2)
-print(buy2[buy2['BTC-USD']])
+adx_buy= adx_pandas_ta.dmp_above(adx_pandas_ta.dmn)
+# adx_buy.columns = buy2.columns.droplevel(0)
+print(f"\ndmp > dmn")
+print(adx_buy.tail())
 
 ce = vbt.pandas_ta('chandelier_exit').run(high, low, close, multiplier=3)
+print(f"\nChandelier Exit")
+ce_signal = ce.chdlrextd > 0
 
-print(dir(ce))
-print(ce.chdlrextl)
-print(ce.chdlrextd)
+print(ce_signal.tail())
+
+entries = sar_buy & adx_buy
+
+exits = ce_signal
+
+print(f"\nentries")
+print(entries.tail())
+
+
+pf = vbt.Portfolio.from_signals(close,
+                                entries=entries,
+                                exits=exits,
+                                size=1,
+                                direction='longonly',
+                                sl_stop=0.1,
+                                sl_trail=True,
+                                freq='4h',
+                                init_cash = 1000,
+                                )
+
+pf_btc = pf[pf.wrapper.columns[0]]
+pf_btc.plot().show()
+
+print(pf_btc.stats())
+
